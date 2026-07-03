d56758a6b9be03e539ac8c8f28e005378d3e6d17
Matthias Colin <colinmatthias9@gmail.com>
2026-07-03 15:09:48 +0200

    gitignore

--- ApiLicensing/DTO/EvolutionDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApiLicensing.DTO
-{
-    public class EvolutionDTO
-    {
-        public string Mois { get; set; } = string.Empty;
-        public double NombreAbos { get; set; }
-    }
-}
--- ApiLicensing/DTO/RenouvellementDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiLicensing.DTO
-{
-    public class RenouvellementDTO
-    {
-        public string Client { get; set; } = string.Empty;
-        public string DateRenouvellement { get; set; } = string.Empty;
-        public string Statut { get; set; } = string.Empty; // "Actif", "EnRetard", "Bientot"
-    }
-}
